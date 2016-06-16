f8a55f8097dbc0ff04302536267704d9a2737955
Eliza Brock Marcum <eliza@elizabrocksoftware.com>
2016-06-16 09:49:14 -0500

    Day 4, Part 2

--- lib/day4.rb
+++ lib/day4.rb
@@ -2,15 +2,17 @@ require 'digest'
 
 class Day4
   def self.run
-    result = Day4.find_hash_salt_for_key('yzbqklnj')
-    puts "The hash salt for part 1 is #{result}"
+    result = Day4.find_hash_salt_for_key('yzbqklnj', leading_zeroes: 5)
+    puts "  The hash salt for part 1 is #{result}"
+    result = Day4.find_hash_salt_for_key('yzbqklnj', leading_zeroes: 6)
+    puts "  The hash salt for part 2 is #{result}"
   end
 
-  def self.find_hash_salt_for_key(key)
-    leading_zeros = '0' * 5
+  def self.find_hash_salt_for_key(key, leading_zeroes: 5)
+    leading_zeroes = '0' * leading_zeroes
     salt = 0
     result = ''
-    until result.start_with?(leading_zeros) do
+    until result.start_with?(leading_zeroes) do
       salt += 1
       result = Digest::MD5.hexdigest("#{key}#{salt}")
     end

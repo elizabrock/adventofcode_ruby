bda30be06d33fac5c8d87458d622544bb15b1076
Eliza Brock Marcum <eliza@elizabrocksoftware.com>
2016-06-24 11:28:53 -0500

    Day 5, Part 2

--- lib/day5.rb
+++ lib/day5.rb
@@ -1,35 +1,59 @@
 class Day5
   def self.run
     word_list = File.open('input/day5_input.txt', 'r').read().split("\n")
-    nice_word_count = word_list.count{ |word| Day5.is_nice? word }
-    puts "  There are #{nice_word_count} nice words in the list"
+    nice_word_count = word_list.count{ |word| Day5::Part1.is_nice? word }
+    puts "  According to the original rules, there are #{nice_word_count} nice words in the list"
+    nice_word_count = word_list.count{ |word| Day5::Part2.is_nice? word }
+    puts "  According to the new rules, there are #{nice_word_count} nice words in the list"
   end
 
-  def self.is_nice? word
-    vowel_count = word.chars.count{ |char| VOWELS.include? char }
-    if vowel_count < 3
-      return false
-    end
-
-    includes_double_letters = false
-    (0..word.chars.count - 1).each do |i|
-      if word[i] == word[i+1]
-        includes_double_letters = true
+  class Part1
+    def self.is_nice? word
+      vowel_count = word.chars.count{ |char| VOWELS.include? char }
+      if vowel_count < 3
+        return false
       end
-    end
-    unless includes_double_letters
-      return false
+
+      includes_double_letters = false
+      (0..word.chars.count - 1).each do |i|
+        if word[i] == word[i+1]
+          includes_double_letters = true
+        end
+      end
+      unless includes_double_letters
+        return false
+      end
+
+      if DISALLOWED_STRINGS.any?{ |string| word.include? string }
+        return false
+      end
+
+      true
     end
 
-    if DISALLOWED_STRINGS.any?{ |string| word.include? string }
-      return false
-    end
+    private
 
-    true
+    VOWELS = "aeiou"
+    DISALLOWED_STRINGS = ["ab", "cd", "pq", "xy"]
   end
 
-  private
+  class Part2
+    def self.is_nice?(word)
+      double_pair_present = false
+      word.chars.each_cons(2) do |pair|
+        if word.scan(pair.join).length > 1
+          double_pair_present = true
+        end
+      end
 
-  VOWELS = "aeiou"
-  DISALLOWED_STRINGS = ["ab", "cd", "pq", "xy"]
+      sandwich_letter_present = false
+      (0..word.chars.count - 2).each do |i|
+        if word[i] == word[i+2]
+          sandwich_letter_present = true
+        end
+      end
+
+      return double_pair_present && sandwich_letter_present
+    end
+  end
 end
--- spec/day5_spec.rb
+++ spec/day5_spec.rb
@@ -1,55 +1,82 @@
 require_relative "spec_helper"
 
 describe Day5 do
-  describe ".is_nice?" do
-    describe "with  at least three vowels, a double letter, and no disallowed substrings" do
-      it "is true" do
-        Day5.is_nice?("ugknbfddgicrmopn").must_equal true
+  describe Day5::Part1 do
+    describe ".is_nice?" do
+      describe "with  at least three vowels, a double letter, and no disallowed substrings" do
+        it "is true" do
+          Day5::Part1.is_nice?("ugknbfddgicrmopn").must_equal true
+        end
+      end
+      describe "that follows all the rules, with different rule letters overlapping" do
+        it "is true" do
+          Day5::Part1.is_nice?("aaa").must_equal true
+        end
+      end
+      describe "that has no double letters" do
+        it "is false" do
+          Day5::Part1.is_nice?("jchzalrnumimnmhp").must_equal false
+        end
+      end
+      describe "that contains disallowed string, ab" do
+        it "is false" do
+          Day5::Part1.is_nice?("haegwjzuvuyypabu").must_equal false
+        end
+      end
+      describe "that contains disallowed string, cd" do
+        it "is false" do
+          Day5::Part1.is_nice?("haegwjzuvuyypcdu").must_equal false
+        end
+      end
+      describe "that contains disallowed string, pq" do
+        it "is false" do
+          Day5::Part1.is_nice?("haegwjzuvuyyppqu").must_equal false
+        end
+      end
+      describe "that contains disallowed string, xy" do
+        it "is false" do
+          Day5::Part1.is_nice?("haegwjzuvuyypxyu").must_equal false
+        end
+      end
+      describe "that has no vowels" do
+        it "is false" do
+          Day5::Part1.is_nice?("dvszwmrrgswjxmb").must_equal false
+        end
+      end
+      describe "that has only one vowel" do
+        it "is false" do
+          Day5::Part1.is_nice?("dvszwmarrgswjxmb").must_equal false
+        end
+      end
+      describe "that has only two vowels" do
+        it "is false" do
+          Day5::Part1.is_nice?("dvszwmarrgswjxmib").must_equal false
+        end
       end
     end
-    describe "that follows all the rules, with different rule letters overlapping" do
-      it "is true" do
-        Day5.is_nice?("aaa").must_equal true
+  end
+
+  describe Day5::Part2 do
+    describe ".is_nice?" do
+      describe "with a pair that appears twice, and a letter that makes a sandwich" do
+        it "is true" do
+          Day5::Part2.is_nice?("qjhvhtzxzqqjkmpb").must_equal true
+        end
       end
-    end
-    describe "that has no double letters" do
-      it "is false" do
-        Day5.is_nice?("jchzalrnumimnmhp").must_equal false
+      describe "the follows all the rules, with different rule letters overlapping" do
+        it "is true" do
+          Day5::Part2.is_nice?("xxyxx").must_equal true
+        end
       end
-    end
-    describe "that contains disallowed string, ab" do
-      it "is false" do
-        Day5.is_nice?("haegwjzuvuyypabu").must_equal false
+      describe "that has no sandwich letter" do
+        it "is false" do
+          Day5::Part2.is_nice?("uurcxstgmygtbstg").must_equal false
+        end
       end
-    end
-    describe "that contains disallowed string, cd" do
-      it "is false" do
-        Day5.is_nice?("haegwjzuvuyypcdu").must_equal false
-      end
-    end
-    describe "that contains disallowed string, pq" do
-      it "is false" do
-        Day5.is_nice?("haegwjzuvuyyppqu").must_equal false
-      end
-    end
-    describe "that contains disallowed string, xy" do
-      it "is false" do
-        Day5.is_nice?("haegwjzuvuyypxyu").must_equal false
-      end
-    end
-    describe "that has no vowels" do
-      it "is false" do
-        Day5.is_nice?("dvszwmrrgswjxmb").must_equal false
-      end
-    end
-    describe "that has only one vowel" do
-      it "is false" do
-        Day5.is_nice?("dvszwmarrgswjxmb").must_equal false
-      end
-    end
-    describe "that has only two vowels" do
-      it "is false" do
-        Day5.is_nice?("dvszwmarrgswjxmib").must_equal false
+      describe "that has no double pair" do
+        it "is false" do
+          Day5::Part2.is_nice?("ieodomkazucvgmuy").must_equal false
+        end
       end
     end
   end
